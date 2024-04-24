abb979a31a429ca14777bee5e9152a54890a9e94
Hugo Rodrigues <hugo.rodrigues@openbit.tech>
2024-04-24 17:10:41 +0100

    Added support to custom element attributes

--- yail.js
+++ yail.js
@@ -7,15 +7,17 @@ const YailClass = (function() {
     const urlParams = new URLSearchParams(window.location.search);
     const userLang = urlParams.get("lang") || navigator.language || navigator.userLanguage;
     const siteLang = document.getElementsByTagName("html")[0].getAttribute("lang") || "en-gb";
+    const defaultAttribute = "i18n"
 
     var YailClass = function() {
 
         this.terms = {};
         this.strict = false;
 
-        this.init = function(terms, strict=false) {
+        this.init = function(terms, strict=false, attribute=defaultAttribute) {
             this.terms = terms;
             this.strict = strict;
+            this.attribute = attribute;
             console.debug(`User language is: ${userLang}`);
             console.debug(`Site language is: ${siteLang}`);
             console.debug(`Strict mode is: ${this.strict}`);
@@ -27,10 +29,10 @@ const YailClass = (function() {
             if (siteLang != userLang && siteLang.substring(0, 2) != userLang.substring(0, 2)) {
                 console.debug("Translating terms");
 
-                const i18n_elements = document.querySelectorAll("[i18n]");
+                const i18n_elements = document.querySelectorAll(`[${this.attribute}]`);
                 for(let i = 0; i< i18n_elements.length; i++) {
                     let element = i18n_elements[i];
-                    let placeholder = element.getAttribute("i18n");
+                    let placeholder = element.getAttribute(this.attribute);
 
                     if (placeholder) {
                         // Using en-GB as a example, it will try en-GB and then just en
