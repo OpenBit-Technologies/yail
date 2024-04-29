b9346e1b0a46b00219c0bdd77b71857b9508cefe
Hugo Rodrigues <hugo.rodrigues@openbit.tech>
2024-04-29 17:09:39 +0100

    Improved fetch of translated term

--- yail.js
+++ yail.js
@@ -82,6 +82,8 @@ class Yail {
             language = this.userLang;
         }
 
+        const lang_code = language.substring(0, 2);
+
         if (!this.check_language(language)) {
             console.error(`No terms for language ${language}`);
             return false;
@@ -98,12 +100,14 @@ class Yail {
                 if (placeholder) {
                     let text = null;
                     try {
-                        text = this.terms[language][placeholder] || this.terms[language.substring(0 ,2)][placeholder];
-                    } catch(err) {
-                        if (err instanceof TypeError) {
-                            console.error(`No mapping for ${placeholder}`);
-                        } else {
-                            console.error(err);
+                        text = this.terms[language][placeholder];
+                    } catch {
+                        if (!this.strict) {
+                            try {
+                                text = this.terms[lang_code][placeholder];
+                            } catch {
+                                console.error(`No mapping for ${placeholder}`);
+                            }
                         }
                     }
                     if (text) {
